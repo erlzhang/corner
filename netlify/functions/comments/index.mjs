b5875f7ec76a39c95c929d7737198cd4540ca433
erlzhang <zhangshiyu1992@hotmail.com>
2024-12-31 18:50:32 +0800

    update response

--- netlify/functions/comments/index.mjs
+++ netlify/functions/comments/index.mjs
@@ -12,20 +12,9 @@ export default async (event, context) => {
             const res = await fetch(BASE_URL + '/comments/' + id, {method: 'POST', body: JSON.stringify(body)});
             console.log('res', res)
             const data = await res.json();
-            console.log('data', data)
-            if (data.code) {
-                return {
-                    statusCode: 200,
-                    body: ''
-                };
-            } else {
-                return {
-                    statusCode: 500,
-                    body: ''
-                }
-            }
+            return new Response(JSON.stringify(data), { status: 200 });
         } catch(error) {
-            return { statusCode: 500, body: error.toString() };
+            return new Response(error.toString(), { status: 500 });
         }
     } else {
     
@@ -33,12 +22,9 @@ export default async (event, context) => {
             const res = await fetch(BASE_URL + '/comments/' + id);
             console.log('res', res)
             const data = await res.json();
-            return {
-                statusCode: 200,
-                body: JSON.stringify(data)
-            };
+            return new Response(JSON.stringify(data), { status: 200 });
         } catch(error) {
-            return { statusCode: 500, body: error.toString() };
+            return new Response(error.toString(), { status: 500 });
         }
     }
 
